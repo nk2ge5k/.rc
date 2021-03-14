4aef544291357df2c6eff25c096129e13bfda1e0
nk2ge5k <nk2ge5k@gmail.com>
2021-03-14 04:42:18 +0300

    improve telescope module

--- vim/lua/custom/telescope.lua
+++ vim/lua/custom/telescope.lua
@@ -1,4 +1,10 @@
 local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local pickers = require('telescope.pickers')
+local previewers = require('telescope.previewers')
+local finders = require('telescope.finders')
+
+local conf = require('telescope.config').values
 
 require('telescope').setup{
     defaults = {
@@ -30,10 +36,40 @@ function exists(name)
     return os.rename(name,name) and true or false
 end
 
+local uservices_dir = home_dir .. "/src/github.yandex-team.ru/nk2ge5k/uservices"
+
 local configs = {
     {
         name = "uservices",
-        path = home_dir .. "/src/github.yandex-team.ru/nk2ge5k/uservices"
+        path = uservices_dir
+    },
+    {
+        name = "eats-catalog",
+        path = uservices_dir .. "/services/eats-catalog"
+    },
+    {
+        name = "eats-layout-constructor",
+        path = uservices_dir .. "/services/eats-layout-constructor"
+    },
+    {
+        name = "eats-communications",
+        path = uservices_dir .. "/services/eats-communications"
+    },
+    {
+        name = "eats-collections",
+        path = uservices_dir .. "/services/eats-collections"
+    },
+    {
+        name = "scripts",
+        path = home_dir .. "/scripts"
+    },
+    {
+        name = "scratch",
+        path = home_dir .. "/scratch"
+    },
+    {
+        name = "github",
+        path = home_dir .. "/src/github.com/nk2ge5k"
     },
     {
         name = "backend_platform",
@@ -41,32 +77,66 @@ local configs = {
     }
 }
 
-
 local custom = {}
 
-for _, config in ipairs(configs) do
-    -- TODO: may be make remaps in lua instead of vimrc
-    custom[config.name .. "_files"] = function()
+custom.projects = function(opts)
+    local projects = {}
+
+    for _, config in ipairs(configs) do
         if exists(config.path) then
-            require("telescope.builtin").find_files({
-                prompt_title = config.name .. " files",
-                cwd = config.path,
+            table.insert(projects, {
+                name = config.name,
+                path = config.path,
             })
-        else
-            print('directory ' .. config.path .. ' does not exists')
         end
     end
 
-    custom[config.name .. "_grep"] = function()
-        if exists(config.path) then
-            require("telescope.builtin").live_grep({
-                prompt_title = config.name .. " grep",
-                cwd = config.path,
-            })
-        else
-            print('directory ' .. config.path .. ' does not exists')
-        end
+    if vim.tbl_isempty(projects) then
+        return
     end
+
+    pickers.new(opts, {
+        prompt_title = 'Projects',
+        finder = finders.new_table {
+            results = projects,
+            entry_maker = function(config)
+                return {
+                    ordinal = config.name,
+                    display = config.name,
+                    filename = config.path,
+                }
+            end
+        },
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            local grep = function()
+                local selection = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
+                local path = selection.filename
+
+                require("telescope.builtin").live_grep({
+                    prompt_title = path .. " grep",
+                    cwd = path,
+                })
+            end
+
+            map('i', '<c-g>', grep)
+            map('n', 'g', grep)
+
+
+            actions.select_default:replace(function()
+                local selection = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
+                local path = selection.filename
+
+                require("telescope.builtin").find_files({
+                    prompt_title = path .. " files",
+                    cwd = path,
+                })
+            end)
+            return true
+        end
+    }):find()
 end
 
 return custom

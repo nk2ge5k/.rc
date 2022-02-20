2ceb35f21749a5a96faef437f84c5ccbb89c2d6e
nk2ge5k <nk2ge5k@gmail.com>
2022-02-20 16:49:32 +0300

    Clean up zsh configuration

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias vim=nvim
+
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias grep='rg --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
---
+++ zsh/env.zsh
@@ -0,0 +1,26 @@
+source "$HOME/.cargo/env"
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# default editor is nvim
+export EDITOR='nvim'
+# change man pager to nvim
+export MANPAGER="nvim -c 'set ft=man' -"
+
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+
+export GOPATH=$HOME
+
+# Add libraries path to linker
+export LD_LIBRARY_PATH="/usr/local/lib"
+
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/bin/go/bin:$PATH"
+export PATH="$HOME/scripts/enabled/:$PATH"
+
+export XDG_CONFIG_HOME=$HOME/.config
+
+export GPG_TTY=`tty`

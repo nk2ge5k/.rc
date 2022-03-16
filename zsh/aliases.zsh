331d1900ba3650c720b1c21744f74acfcc7b2a97
Georgy Chernukhin <nk2ge5k@sandbox-633380738>
2022-03-16 21:29:35 +0300

    enable exa and disable compinit

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,15 @@
-alias vim=nvim
+if [ -x "$(command -v nvim)" ]; then
+    alias vim=nvim
+fi
 
-alias ls='ls --color=auto'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+if ! command -v exa &> /dev/null
+then
+    alias ls='ls --color=auto'
+    alias ll='ls -alF'
+else
+    alias ls='exa -s type'
+    alias ll='exa -alGF -s type'
+fi
 
 alias grep='rg --color=auto'
 alias fgrep='fgrep --color=auto'

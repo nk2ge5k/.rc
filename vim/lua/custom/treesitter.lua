44960e682f8a477383cfdabad49c1cc6c3038af9
nk2ge5k <nk2ge5k@gmail.com>
2022-04-25 13:59:16 +0300

    clean up

--- vim/lua/custom/treesitter.lua
+++ vim/lua/custom/treesitter.lua
@@ -1,3 +1,4 @@
+
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "maintained",
     ignore_install = {},
@@ -14,6 +15,11 @@ require'nvim-treesitter.configs'.setup {
         enable = true,
         disable = {},
         additional_vim_regex_highlighting = false
+    },
+    query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = {"BufWrite", "CursorHold"}
     },
     playground = {
         enable = false,

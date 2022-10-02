474d5ae8382aade897cfa4c016f5fa63e43c98b1
nk2ge5k <nk2ge5k@gmail.com>
2022-10-02 23:07:31 +0300

    Set command line height to zero on neovim version above 0.8.0

--- vim/lua/custom/init.lua
+++ vim/lua/custom/init.lua
@@ -6,7 +6,14 @@ notify.setup({
   timeout = 4000,
 })
 
+-- Setup notification engine
 vim.notify = notify
+
+local version = vim.version()
+if version.major >= 0 and version.minor >= 8 then
+  -- Set command line height to zero on verions >=0.8.x
+  vim.o.ch = 0
+end
 
 require("custom.keymaps")
 require("custom.nvim-lsp")

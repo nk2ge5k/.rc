30c4659be7da2ebc7ef69728261ef0f8875edb04
nk2ge5k <nk2ge5k@gmail.com>
2024-01-25 15:02:41 +0400

    Remove more plugins

--- vim/lua/custom.lua
+++ vim/lua/custom.lua
@@ -1,5 +1,4 @@
 local notify = require("notify")
-local null_ls = require("null-ls")
 
 notify.setup({
   icons = {
@@ -21,17 +20,6 @@ if version.major >= 0 and version.minor >= 8 then
   -- Set command line height to zero on versions >=0.8.x
   vim.o.ch = 0
 end
-
-null_ls.setup({
-  sources = {
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.code_actions.shellcheck,
-    -- python
-    null_ls.builtins.formatting.black.with { extra_args = { "--fast" } },
-    null_ls.builtins.formatting.isort,
-  },
-})
 
 require("custom.keymaps")
 require("custom.nvim-lsp")
--- vim/lua/custom/nvim-lsp.lua
+++ vim/lua/custom/nvim-lsp.lua
@@ -1,40 +1,4 @@
 local nvim_lsp = require('lspconfig')
-local cmp = require("cmp")
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-u>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    {
-      name = 'buffer',
-      option = {
-        get_bufnrs = function()
-          local bufs = {}
-          for _, win in ipairs(vim.api.nvim_list_wins()) do
-            bufs[vim.api.nvim_win_get_buf(win)] = true
-          end
-          return vim.tbl_keys(bufs)
-        end,
-      },
-    },
-    { name = 'luasnip' },
-  })
-})
 
 local is_file_exists = function(file_name)
   local file = io.open(file_name, "r")
@@ -48,32 +12,6 @@ end
 
 local startswith = function(text, prefix)
   return text:find(prefix, 1, true) == 1
-end
-
-local on_attach = function(_, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-  vim.keymap.set("n", "gv", vim.lsp.buf.definition, { buffer = 0 })
-  vim.keymap.set('n', '<leader>v', ':vsplit | lua vim.lsp.buf.definition()<CR>')
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-  vim.keymap.set("n", "gr", vim.lsp.buf.references)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-  vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
-  vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist)
-  vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-  vim.keymap.set("n", "<leader>H", vim.diagnostic.goto_prev)
-  vim.keymap.set("n", "<leader>L", vim.diagnostic.goto_next)
-
-  local filetype = vim.bo.filetype
-  if filetype == "go" then
-    vim.keymap.set("n", "<leader>E", function()
-      vim.lsp.buf_request_sync(bufnr, "workspace/executeCommand", {
-        command = "gopls.gc_details",
-        arguments = { vim.uri_from_bufnr(bufnr) },
-      }, 2000)
-    end)
-  end
 end
 
 --- clangd -------------------------------------------------------
@@ -91,7 +29,6 @@ end
 local setup_clangd = function(lsp)
   lsp.clangd.setup {
     autostart = true,
-    on_attach = on_attach,
     cmd = clangd_command(),
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }
   }
@@ -106,7 +43,6 @@ local setup_lua = function(lsp)
   table.insert(runtime_path, "lua/?/init.lua")
 
   lsp.lua_ls.setup {
-    on_attach = on_attach,
     settings = {
       Lua = {
         runtime = {
@@ -138,7 +74,6 @@ end
 
 local setup_gopls = function(lsp)
   lsp.gopls.setup {
-    on_attach = on_attach,
     settings = {
       gopls = {
         analyses = {
@@ -163,7 +98,6 @@ end
 
 local setup_rust_analyzer = function(lsp)
   lsp.rust_analyzer.setup({
-    on_attach = on_attach,
     settings = {
       ["rust-analyzer"] = {
         imports = {
@@ -185,6 +119,14 @@ local setup_rust_analyzer = function(lsp)
   })
 end
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = false,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
 local servers = {
   "pyright",
   "tsserver",
@@ -195,7 +137,7 @@ local servers = {
 }
 
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
+  nvim_lsp[lsp].setup {}
 end
 
 local custom = {
@@ -212,10 +154,33 @@ for _, fn in ipairs(custom) do
   fn(nvim_lsp)
 end
 
-vim.diagnostic.config({
-  virtual_text = true,
-  signs = false,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
 })
